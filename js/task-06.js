b200f71391efc3b0d97ba6c93ba1cb5458b52257
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-02-28 10:53:34 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,8 @@
+const textInput = document.querySelector('#validation-input');
+
+textInput.addEventListener('blur', () => {
+    textInput.classList.toggle('valid', textInput.value.length >= 6);
+    textInput.classList.toggle('invalid', textInput.value.length <= 6);
+})
+
+
